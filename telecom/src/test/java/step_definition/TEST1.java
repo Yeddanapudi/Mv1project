ff740d858a51106ed3495d5d65cfd0b114ed1f11
Yeddanapudi <yeddanapudi1979@yahoo.com>
2023-05-12 15:51:41 +0530

    WELCOME

--- telecom/src/test/java/step_definition/TEST1.java
+++ telecom/src/test/java/step_definition/TEST1.java
@@ -4,7 +4,7 @@ public class TEST1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("welcome");
+System.out.println("welcome TO TESTING 123");
 
 	}
 
